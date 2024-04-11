9d897202c7a5396f70279bce123bbfa4eb7c961d
komeponpon <ricemountain1030@gmail.com>
2024-04-11 14:39:18 +0900

    バックエンドテスト

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,1 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const server = http_1.default.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+});
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
